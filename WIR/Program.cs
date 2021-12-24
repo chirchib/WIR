be73fda1284ee6003f610640cd7804472c7c502b
chirchib <dmitry.kurochkin66@gmail.com>
2021-12-24 15:18:39 +0300

    Add project files.

---
+++ WIR/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Threading;
+
+// Поиск подстроки в строке с помощью суффиксного дерева (Suffix tree)  (Suffix tree)
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int SIZE = 10000;
+            string[] Text = new string[SIZE];
+
+            Console.WriteLine("Размер массива: {0}", SIZE);
+
+            int min_size = 5;
+            int max_size = 10;
+
+            Random random = new Random();
+            char[] ABC = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+            for (int i = 0; i < Text.Length; i++)
+            {
+                for (int j = 0; j < random.Next(min_size, max_size); j++)
+                {
+                    Text[i] += ABC[random.Next(0, ABC.Length - 1)];
+                }
+            }
+
+            //DisplayStringArray(Text);
+
+            string substr = "ababa";
+            Console.WriteLine("Подстрока: " + substr);
+
+            Timer timer1 = new Timer();
+            timer1.Start();
+            foreach (var str in Text)
+            {
+                BruteForceAlgorithm(str, substr);
+            }
+            timer1.Stop();
+            Console.WriteLine("Время поиска наивного алгоритма: " + timer1.Duration.ToString());
+
+
+            Timer timer2 = new Timer();
+            timer2.Start();
+            foreach (var str in Text)
+            {
+                SuffixTree tree = new SuffixTree(str);
+                tree.Find(substr);
+            }
+            timer2.Stop();
+            Console.WriteLine("Время поиска с помощью суффиксного дерева: " + timer2.Duration.ToString());
+
+
+            Console.ReadKey();
+        }
+
+        /// Вывод в консоль массив
+        static void DisplayStringArray(string[] array)
+        {
+            foreach (var str in array)
+            {
+                Console.WriteLine(str);
+            }
+        }
+
+        /// нативный алгоритм, возвращает true если в строке есть вхождение указанной подстроки
+        static bool BruteForceAlgorithm(string str, string substring)
+        {
+            for (int i = 0; i <= str.Length - substring.Length; i++)
+            {
+                bool success = true;
+                for (int j = 0; j < substring.Length; j++)
+                {
+                    if (str[i + j] != substring[j])
+                    {
+                        success = false;
+                        break;
+                    }
+                }
+                if (success) return true;
+            }
+            return false;
+        }
+
+
+    }
+
+    public class SuffixTree
+    {
+        public class Node
+        {
+            public int Index = -1;
+            public Dictionary<char, Node> Children = new Dictionary<char, Node>();
+        }
+
+        public Node Root = new Node();
+        public String Text;
+
+        public void InsertSuffix(string s, int from)
+        {
+            var cur = Root;
+            for (int i = from; i < s.Length; ++i)
+            {
+                var c = s[i];
+                if (!cur.Children.ContainsKey(c))
+                {
+                    var n = new Node() { Index = from };
+                    cur.Children.Add(c, n);
+
+
+
+                    return;
+                }
+                cur = cur.Children[c];
+            }
+        }
+
+        private static IEnumerable<Node> VisitTree(Node n)
+        {
+            // yield return: определяет возвращаемый элемент
+            // yield break: указывает, что последовательность больше не имеет элементов
+            foreach (var n1 in n.Children.Values)
+                foreach (var n2 in VisitTree(n1))
+                    yield return n2;
+            yield return n;
+        }
+
+        public IEnumerable<int> Find(string s)
+        {
+            var n = FindNode(s);
+            if (n == null) yield break;
+            foreach (var n2 in VisitTree(n))
+                yield return n2.Index;
+        }
+
+        private Node FindNode(string s)
+        {
+            var cur = Root;
+            for (int i = 0; i < s.Length; ++i)
+            {
+                var c = s[i];
+                if (!cur.Children.ContainsKey(c))
+                {
+                    // Мы находимся на листе-узле.
+                    // Здесь мы проверяем, находится ли остальная часть строки в этом месте.
+                    for (var j = i; j < s.Length; ++j)
+                        if (cur.Index + j >= Text.Length || Text[cur.Index + j] != s[j])
+                            return null;
+                    return cur;
+                }
+                cur = cur.Children[c];
+            }
+            return cur;
+        }
+
+        public SuffixTree(string s)
+        {
+            Text = s;
+            for (var i = s.Length - 1; i >= 0; --i)
+                InsertSuffix(s, i);
+        }
+    }
+
+    class Timer
+    {
+        [DllImport("Kernel32.dll")]
+        private static extern bool QueryPerformanceCounter(out long lpPerformanceCount);
+
+        [DllImport("Kernel32.dll")]
+        private static extern bool QueryPerformanceFrequency(out long lpFrequency);
+
+        private long startTime, stopTime;
+        private long freq;
+
+        public Timer()
+        {
+            startTime = 0;
+            stopTime = 0;
+
+            if (QueryPerformanceFrequency(out freq) == false)
+            {
+                Console.WriteLine("Таймер не поддерживается	аппаратной частью компьютера.");
+            }
+
+        }
+
+        public double Start()
+        {
+            Thread.Sleep(0);
+            QueryPerformanceCounter(out startTime);
+            return 1.0 * startTime / freq;
+        }
+
+        public double Stop()
+        {
+            QueryPerformanceCounter(out stopTime);
+            return 1.0 * stopTime / freq;
+        }
+
+        public double Duration
+        {
+            get
+            {
+                return (double)(stopTime - startTime) / (double)freq;
+            }
+        }
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BKTree tree = new BKTree();
+
+            string[] dictionary = { "hell","help","shell","smell",
+                                    "fell","felt","oops","pop","oouch","halt"
+                                  };
+
+            tree.LoadDictionary(dictionary);
+
+            // Совпадения поиск help
+            string str = "felj";
+            IEnumerable<string> result = tree.GetSpellingSuggestions(str);
+
+            foreach (string s in dictionary)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Совпадения: " + str);
+            foreach (string s in result)
+            {
+                Console.Write(s + " ");
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+
+    class BKTree
+    {
+        private BKNode m_root = null;
+        private static int s_defaultTolerance = 2;
+
+        // Добавляет новое слово в дерево
+        public void AddWord(string word)
+        {
+            if (m_root == null)
+            {
+                m_root = new BKNode(word);
+            }
+            else
+            {
+                m_root.AddNode(word);
+            }
+        }
+
+        public bool WordExists(string word)
+        {
+            return m_root != null ? m_root.FindWord(word) : false;
+        }
+
+        public IEnumerable<string> GetSpellingSuggestions(string word)
+        {
+            return GetSpellingSuggestions(word, s_defaultTolerance);
+        }
+
+        public IEnumerable<string> GetSpellingSuggestions(string word, int tolerance)
+        {
+            if (m_root == null) return null;
+
+            List<string> suggestions = new List<string>();
+            suggestions.AddRange(m_root.FindSuggestions(word, tolerance));
+            return suggestions;
+        }
+
+
+        public void LoadDictionary(string[] Text)
+        {
+
+            for (int i = 0; i < Text.Length; i++)
+            {
+                AddWord(Text[i]);
+            }
+        }
+
+
+
+        private class BKNode
+        {
+            private string m_word;
+            private Dictionary<int, BKNode> m_children;
+
+            public BKNode(string word)
+            {
+                m_word = word;
+                m_children = new Dictionary<int, BKNode>();
+            }
+
+            public void AddNode(string word)
+            {
+                int dist = Levenshtein.Compute(word, m_word);
+
+                // Слово уже в словаре!
+                if (dist == 0) return;
+
+                BKNode child;
+                if (m_children.TryGetValue(dist, out child))
+                {
+                    // У нас уже есть еще один узел на таком же расстоянии, поэтому добавьте это слово в поддерево этого узла.
+                    child.AddNode(word);
+                }
+                else
+                {
+                    // У нас нет ребенка на таком расстоянии, поэтому добавляется новый ребенок
+                    m_children.Add(dist, new BKNode(word));
+                }
+            }
+
+            public bool FindWord(string word)
+            {
+                int dist = Levenshtein.Compute(word, m_word);
+
+                // Слово уже в словаре!
+                if (dist == 0) return true;
+
+                BKNode child;
+                if (m_children.TryGetValue(dist, out child))
+                {
+                    // У нас уже есть еще один узел на таком же расстоянии, поэтому добавьте это слово в поддерево этого узла.
+                    return child.FindWord(word);
+                }
+
+                return false;
+            }
+
+            public IEnumerable<string> FindSuggestions(string word, int tolerance)
+            {
+                List<string> suggestions = new List<string>();
+
+                int dist = Levenshtein.Compute(word, m_word);
+                if (dist < tolerance)
+                {
+                    suggestions.Add(m_word);
+                }
+
+                foreach (int key in m_children.Keys)
+                {
+                    if (key >= dist - tolerance && key <= dist + tolerance)
+                    {
+                        suggestions.AddRange(m_children[key].FindSuggestions(word, tolerance));
+                    }
+                }
+
+                return suggestions;
+            }
+        }
+    }
+
+    static class Levenshtein
+    {
+        public static int Compute(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            if (n == 0)
+            {
+                return m;
+            }
+
+            if (m == 0)
+            {
+                return n;
+            }
+
+            for (int i = 0; i <= n; d[i, 0] = i++) ;
+            for (int j = 0; j <= m; d[0, j] = j++) ;
+
+            for (int i = 1; i <= n; ++i)
+            {
+                for (int j = 1; j <= m; ++j)
+                {
+                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+                    d[i, j] = Math.Min(
+                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                        d[i - 1, j - 1] + cost);
+                }
+            }
+
+            return d[n, m];
+        }
+    }
+}
